48dc854796d234920778c3e1669483328c808e16
Gopi2466 <gopi2466@gmail.com>
2024-08-13 15:56:54 -0400

    create input validataion

--- Backend/Middlewares/user.js
+++ Backend/Middlewares/user.js
@@ -1,3 +1,25 @@
+const zod = require("zod");
+
+const inputSchema = zod.object({
+    username: zod.string().email(),
+    password: zod.string().min(8, {message:"Must be 8 or more characters long"})
+})
+
 function UserMiddleware(req, res, next) {
+// this is a middleware where all the user information is verified and if this is good we call the next() otherwise send an error to the user
+const username = req.body.username;
+const password = req.body.password;
+const response = inputSchema.safeParse({username, password})
+if(!response) {
+    res.status(411).json({message: "Invalid user inputs."}); 
+}
+else {
+    next()
+
+
+}
+//Input validation.
+
+
 
 }
